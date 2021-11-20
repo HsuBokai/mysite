3c58dc4a27d479f7a6f8285799b39b02eec9bec4
HsuBokai <exp2.718281@gmail.com>
2021-11-20 16:48:12 +0800

    add run.sh

---
+++ mysite/run.sh
@@ -0,0 +1,5 @@
+
+# ./run.sh python manage.py runserver 0.0.0.0:8000
+# ./run.sh python manage.py startapp blog
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app -p 8000:8000 --user "$(id -u)":"$(id -g)" testserver:1.0 $@
+
